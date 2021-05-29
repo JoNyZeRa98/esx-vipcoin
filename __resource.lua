fa6920f6a9a5bc676a3ddd619c0a2987ab6a5065
JoNyZeRa98 <43113706+JoNyZeRa98@users.noreply.github.com>
2021-05-29 16:09:28 +0200

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,24 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+
+author 'JoNyZeRa98'
+
+description 'Usable Object that gives a random reward'
+
+version '1.0'
+
+informations 'You can add me on discord for anything: jonyzera#2168'
+
+
+server_scripts {
+	'@mysql-async/lib/MySQL.lua',
+	'@es_extended/locale.lua',
+	'server/main.lua'
+}
+
+client_scripts {
+
+	'@es_extended/locale.lua',
+	'client/main.lua'
+	
+}
